b88337b2994ff5c9f5f652e3e3db5e9fbcfd335d
root <root@DSFirebird>
2017-05-23 00:08:16 +0300

    Complete sorting table OWNER v.0.1

--- Kronwerk_DB_Output.py
+++ Kronwerk_DB_Output.py
@@ -22,14 +22,59 @@ Version = '0.1'
 # 3. Из таблицы OWNER вывести нужные столбцы на экран.
 
 # Подключаем модуль работы с базой данных Firebird.
-import fdb
+import firebirdsql 
 
 # Перечень переменых.
 # -------------------
-# Адрес сервера на котором рсположена база данных.
-# Server = 'localhost'
+# Старая база данных:
+# -------------------
+# Сервер со старой базой данных.
+OFBServer = 'localhost'
+# Путь к старой безе данных.
+OFBPath = '/var/lib/firebird/2.5/data/IBNET.GDB'
+# Администратор старой базы данных.
+OFBUser = 'sysdba'
+# Пароль к учетной записи администратора старой базы данных.
+OFBPassword = 'c38c9f81'
+
+# Не учитывать записи таблицы owner, в которой вместо поля фамилия/имя/отчество
+# присутствуют записи none, РАЗОВЫЙ.
+EmptyOwner = 1 #1 - true, 0 - false
+
+# Счётчик (временная переменная).
+Counter = 1
 
 # Вывод приветствия.
-print ('Скрипт Kronwerk_DB_Output ver.' + Versiona)
+print ('Скрипт Kronwerk_DB_Output ver.' + Version)
 print ('Реализован на языке программирования Python v.3.4.2')
 print ('\n')
+
+# Подключаемся к базе данных.
+OldDB = firebirdsql.connect(dsn = OFBServer + ':' + OFBPath,
+                            user = OFBUser,
+                            password = OFBPassword)
+
+# Делаем запрос в старую базу данных для таблицы owner. 
+OwnerTables = OldDB.cursor()
+
+SELECT = ("SELECT ow_id, ow_firstname, ow_middlename, ow_lastname, " +
+         "ow_sdv_id, ow_ws_id FROM owner")
+
+# Отключение строки с хотя бы одним пустым полем Ф.И.О.
+if (EmptyOwner):
+    SELECT = (SELECT + " WHERE ow_firstname IS NOT NULL AND " +
+              "ow_middlename IS NOT NULL AND ow_lastname IS NOT NULL")
+
+# Сортируем все по столбцу ow_id.
+SELECT = (SELECT + " ORDER BY ow_id")
+OwnerTables.execute(SELECT)
+
+# Выводим на экран нужные колонки таблицы owner.
+for (ow_id, ow_firstname, ow_middlename, ow_lastname, ow_sdv_id,
+     ow_ws_id) in OwnerTables:
+    print (Counter, ow_id, ow_lastname, ow_firstname, ow_middlename,
+           ow_sdv_id, ow_ws_id)
+    Counter = Counter + 1
+
+# Закрываем старую базу данных.
+OldDB.close()
